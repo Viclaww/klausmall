808e164fad8352d39bb6fa534f3ef81c2b8abe15
Victor Lawrence <viclawrence99@gmail.com>
2024-02-20 07:39:20 +0100

    break

--- src/components/HeroComp.vue
+++ src/components/HeroComp.vue
@@ -3,15 +3,16 @@
 </script>
 <template>
   <!-- <SwipeComp /> -->
-  <div class="">
+  <div class="w-full flex gap-4 flex-col md:flex-row-reverse items-center">
     <img class="w-[80%]" src="../assets/pngegg(873) 1.png" alt="" />
-    <div class="text">
+    <div class="text-center flex flex-col w-[70%]">
       <h3 class="text-4xl font-medium">
         Don't be <br />
         on Santa's
         <br />
         Naughty List
       </h3>
+      <p>Share the season of love by giving to family and loved ones</p>
     </div>
   </div>
 </template>
