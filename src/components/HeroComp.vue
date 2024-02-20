04e1e3bef5744efbca9a415e13632918b63d2072
Victor Lawrence <viclawrence99@gmail.com>
2024-02-20 07:35:57 +0100

    Swiper not swiping

--- src/components/HeroComp.vue
+++ src/components/HeroComp.vue
@@ -1,6 +1,17 @@
 <script setup>
-import SwipeComp from './SwipeComp.vue'
+// import SwipeComp from './SwipeComp.vue'
 </script>
 <template>
-  <SwipeComp />
+  <!-- <SwipeComp /> -->
+  <div class="">
+    <img class="w-[80%]" src="../assets/pngegg(873) 1.png" alt="" />
+    <div class="text">
+      <h3 class="text-4xl font-medium">
+        Don't be <br />
+        on Santa's
+        <br />
+        Naughty List
+      </h3>
+    </div>
+  </div>
 </template>
--- src/components/SwipeComp.vue
+++ src/components/SwipeComp.vue
@@ -1,19 +1,5 @@
-<script setup>
-const swipeCard = []
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div class="w-full">
-    <div class="flex justify-evenly flex-row-reverse items-center">
-      <img src="../assets/pngegg(873) 1.png" alt="" />
-      <div class="text">
-        <h2 class="text-7xl leading-tight font-medium">
-          Don't be <br />
-          on Santa's <br />
-          Naughty List
-        </h2>
-        <p class="text-lg">share the seasons love with family friends and loved ones</p>
-      </div>
-    </div>
-  </div>
+  <div>Couldnt swiper</div>
 </template>
