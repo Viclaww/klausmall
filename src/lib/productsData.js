24db1d8e0ddcfc3569e586e144b9430e8e7054db
Victor Lawrence <viclawrence99@gmail.com>
2024-02-24 06:28:48 +0100

    Added products array

---
+++ src/lib/productsData.js
@@ -0,0 +1,37 @@
+const products = [
+  {
+    productName: 'Beverage Hamper',
+    productPrice: 10000,
+    productImg: '',
+    reviews: [],
+    productQuant: 500
+  },
+  {
+    productName: 'Beverage Hamper',
+    productPrice: 10000,
+    productImg: '',
+    reviews: [],
+    productQuant: 500
+  },
+  {
+    productName: 'Beverage Hamper',
+    productPrice: 10000,
+    productImg: '',
+    reviews: [],
+    productQuant: 500
+  },
+  {
+    productName: 'Beverage Hamper',
+    productPrice: 10000,
+    productImg: '',
+    reviews: [],
+    productQuant: 500
+  },
+  {
+    productName: 'Beverage Hamper',
+    productPrice: 10000,
+    productImg: '',
+    reviews: [],
+    productQuant: 500
+  }
+]
